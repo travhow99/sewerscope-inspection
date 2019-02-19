a0ac6717c3f482cb382fc240677ab1d0c08a7653
travis <trav.horn@gmail.com>
2019-02-19 12:42:05 -0700

    Universal selector function

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-  console.log('ready');
 
   $( function() {
     $( "#datepicker" ).datepicker();
@@ -17,19 +16,26 @@ $(document).ready(function() {
       scrollbar: true
   });
 
-  $('#lineCondition3').change(function() {
+  $('select').change(function() {
+    let $this = $(this);
+    let $selected = $this.find(':selected');
 
     const numberInput = `<input type="number" class="number-input">`;
-    let $selected = $('#lineCondition3 option:selected');
-    console.log($(this).siblings('.editing'));
+    //let $selected = $('#lineCondition3 option:selected');
 
     if ($selected.hasClass('number')) {
       $editable = $selected.text().replace("{num}", numberInput);
-      $selected.parent().prev().append(`<p class="editing">${$editable} <span class="remover">x</span></p>`);
+      $selected.parent().prev().after(`<p class="editing">${$editable} <span class="remover">x</span></p>`);
       $(this).hide();
     }
 
-    console.log($('#lineCondition3 option:selected').text());
   });
 
+  $(document).on('click', '.remover', function(){
+    console.log('clicked');
+    let $this = $(this);
+    console.log($this);
+    $this.closest('.editing').siblings('.form-control').show();
+    $this.closest('.editing').hide();
+  });
 });
