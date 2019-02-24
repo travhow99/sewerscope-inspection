7179bb2c8359113a56972d3adf17d73ac3294fa0
travis <trav.horn@gmail.com>
2019-02-24 08:58:34 -0700

    Function to serialize form input fields

--- index.js
+++ index.js
@@ -38,4 +38,61 @@ $(document).ready(function() {
     $this.closest('.editing').siblings('.form-control').show();
     $this.closest('.editing').hide();
   });
+
+
+
+  function generatePDF() {
+    event.preventDefault();
+
+    var x = $("#mainForm").serializeArray();
+    $.each(x, function(i, field){
+      console.log(field.name + ":" + field.value + " ");
+    });
+
+
+/*
+    let $formData = $('#mainForm');
+    console.log($formData);
+    var txt = "";
+    var i;
+    for (i = 0; i < $formData.length; i++) {
+      txt = txt + $formData.elements[i].value + "<br>";
+    }
+    console.log(txt);
+
+
+    console.log('clicked');
+    var doc = new jsPDF()
+
+    doc.text('Hello world!', 10, 10)
+    //doc.save('a4.pdf')
+*/
+  }
+
+  function gatherData() {
+    const $input = '<input type="number" class="number-input">';
+    const $span = ' <span class="remover">x</span>';
+
+    $('.editing').each(function() {
+      console.log($(this));
+      let $inputValue = $(this).children('.number-input').val();
+      let $html = $(this).html();
+
+      let res = $html.replace($input, $inputValue);
+      res = res.replace($span, '');
+
+      console.log($html.indexOf($input));
+      console.log(res);
+      $(this).html(res);
+    });
+  }
+
+  $("#submit").click(function() {
+    // Call function to fix all .editing
+    gatherData();
+
+    generatePDF();
+  });
+
+
 });
