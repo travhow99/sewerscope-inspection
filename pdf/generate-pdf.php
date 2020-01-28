7cc6e55c6e56eee9ad1f5dd781a11e10438219b9
travis <trav.horn@gmail.com>
2020-01-27 18:02:04 -0700

    re-add dompdf...

--- pdf/generate-pdf.php
+++ pdf/generate-pdf.php
@@ -1,9 +1,5 @@
 <?php
-// require_once 'dompdf/autoload.inc.php';
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-die();
+require_once 'dompdf/autoload.inc.php';
 use Dompdf\Dompdf;
 
 
