899a78ab4a5772780d81f15443d5ba6d54f92716
travis <trav.horn@gmail.com>
2020-02-05 13:55:51 -0700

    Cleanup

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-/* if (false) { (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+if (false) { (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
   $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
   header('HTTP/1.1 301 Moved Permanently');
   header('Location: ' . $location);
   exit;
-} */
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
