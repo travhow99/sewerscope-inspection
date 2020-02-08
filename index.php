3ae419e1d99f25fac2657ea308a1398f510f827c
travis <trav.horn@gmail.com>
2020-02-08 10:58:48 -0700

    Updated manifest

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-if (false) { (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
   $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
   header('HTTP/1.1 301 Moved Permanently');
   header('Location: ' . $location);
